2582bdaec8250e5e3b2e969de01a81fd8fc6140e
Graeme Sutherland <grasuth@nodestone.io>
2017-05-19 11:17:52 +0000

    tidying up, doc updates

--- README.md
+++ README.md
@@ -19,9 +19,32 @@ Expects a kinsis stream called 'rawdata' to contain JSON objects of the format
 Then looks up the custId in a DynamoDB table and sums the value in that table
 and increments a counter.
 
+## Building
+Requires Gradle 2.3.
+
+To start:
+
+    gradle clean run
+    
+You'll need to set up:
+* Kinesis stream (called 'rawdata') that provides JSON data as explained above
+* DynamoDB table called 'CustomerSum' containing records for customers with fields:
+*       CustomerId: string
+*       TotalSum: number
+*       Updates: number
+* Appropriate AWS credentials in ~/.aws/credentials
+ 
+TotalSum holds the sum of all customer record values for that CustomerId.
+Updates is a count of updates received.
+
 ## Notes
 
 This is constructed from bits of Amazon Kinseis Client Library examples
 correcting a few issues on the way.  For more info on that see: 
 
 http://docs.aws.amazon.com/streams/latest/dev/kinesis-record-processor-implementation-app-java.html
+
+# License
+
+Free for your use.  I accept no responsibility for it or how you use it. Portions of this 
+from Amazon's samples.  See their licenses in relevant files.
--- src/main/java/com/nodestone/ksum/RecordProcessor.java
+++ src/main/java/com/nodestone/ksum/RecordProcessor.java
@@ -30,6 +30,18 @@ import com.amazonaws.services.kinesis.clientlibrary.interfaces.IRecordProcessorC
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.ShutdownReason;
 import com.amazonaws.services.kinesis.model.Record;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
+
+
 /**
  * Processes records and checkpoints progress.
  */
@@ -47,6 +59,14 @@ public class RecordProcessor implements IRecordProcessor {
     private long nextCheckpointTimeInMillis;
 
     private final CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
+    
+    // DynamoDB Region & endpoint constants
+    private static String DB_REGION = "eu-west-1";
+    private static String DB_ENDPOINT = "dynamodb.eu-west-1.amazonaws.com";
+    private static String DB_TABLE = "CustomerSum";
+    
+    private Table dbTable;
+    private DynamoDB dynamoDB;
 
     /**
      * {@inheritDoc}
@@ -55,6 +75,8 @@ public class RecordProcessor implements IRecordProcessor {
     public void initialize(String shardId) {
         LOG.info("Initializing record processor for shard: " + shardId);
         this.kinesisShardId = shardId;
+        
+        initDBConnection();
     }
 
     /**
@@ -118,16 +140,25 @@ public class RecordProcessor implements IRecordProcessor {
         // TODO Add your own record processing logic here
 
         String data = null;
+        RawData rawData = null;
+        
         try {
             // For this app, we interpret the payload as UTF-8 chars.
             data = decoder.decode(record.getData()).toString();
             
             try {
-                RawData raw = RawData.fromJSON(data);
+                rawData = null;
+                rawData = RawData.fromJSON(data);
             
-                LOG.info("raw: " + raw.custId.toString() + " " + raw.value.toString());
+                LOG.info("raw: " + rawData.custId.toString() + " " + rawData.value.toString());
+                
+                
             } catch (Exception e) {
                 LOG.info("Can't decode raw data as json, ignoring - data was:" + data);
+            }
+            
+            if (rawData != null) {
+                this.updateRecord(rawData);
             }
 
             LOG.info(record.getSequenceNumber() + ", " + record.getPartitionKey() + ", " + data );
@@ -182,6 +213,46 @@ public class RecordProcessor implements IRecordProcessor {
             } catch (InterruptedException e) {
                 LOG.debug("Interrupted sleep", e);
             }
+        }
+    }
+   
+   /**
+    * Initialize DB connection to DynamoDB
+    */
+    private void initDBConnection() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                    DB_ENDPOINT,
+                    DB_REGION))
+            .build();
+
+        this.dynamoDB = new DynamoDB(client);
+
+        this.dbTable = dynamoDB.getTable(DB_TABLE);
+    }
+    
+    /**
+     * Update DynamoDB record:
+     *  - updating updates count
+     *  - updating total sum
+     */
+    private void updateRecord(RawData rawData) {
+        UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+            .withPrimaryKey("CustomerId", rawData.custId)
+            .withUpdateExpression("set TotalSum = TotalSum + :val, Updates = Updates + :inc")
+            .withValueMap(new ValueMap()
+                .withNumber(":val", Integer.parseInt(rawData.value))
+                .withNumber(":inc", 1))
+            .withReturnValues(ReturnValue.UPDATED_NEW);
+            
+        try {
+            UpdateItemOutcome outcome = this.dbTable.updateItem(updateItemSpec);
+            
+            LOG.info("UpdateItem succeeded:\n" + outcome.getItem().toJSONPretty());
+        }
+        catch (Exception e) {
+            LOG.info("Update failed with exception:");
+            LOG.info(e.getMessage());
         }
     }
 }
